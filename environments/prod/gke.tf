dc1fa1477552f1573897f68e77bb202bce4f6976
kajsss <kajsss@gmail.com>
2020-11-27 17:51:47 +0900

    change eshop-app to eshop-apps

--- environments/prod/gke.tf
+++ environments/prod/gke.tf
@@ -83,12 +83,12 @@ module "gke_auth" {
   location     = google_container_cluster.primary.location
 }
  
-resource "kubernetes_namespace" "eshop-app" {
+resource "kubernetes_namespace" "eshop-apps" {
     metadata {
         annotations      = {}
         labels           = {
           istio-injection = "enabled"
         }
-        name             = "eshop-app"
+        name             = "eshop-apps"
     }
 }
